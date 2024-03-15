88967557febeaf8a67161ea4b0b639ea56956562
Prashant Patil <prashantspatil59@gmail.com>
2024-03-15 19:59:37 +0530

    This is 2nd commit

---
+++ src/Practice/LoginPage.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class LoginPage {
+ void loginpage() {
+	 System.out.println("This is login page");
+ }
+public void logoutpage() {
+	System.out.println("This is logout page");
+}
+
+}
