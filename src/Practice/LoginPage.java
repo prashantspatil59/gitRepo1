c716d478cb56d92fb437ba784b800c7d93c90cc3
Prashant Patil <prashantspatil59@gmail.com>
2024-03-16 22:48:18 +0530

    mini

--- src/Practice/LoginPage.java
+++ src/Practice/LoginPage.java
@@ -2,7 +2,7 @@ package Practice;
 
 public class LoginPage {
  void loginpage() {
-	 System.out.println("This is rhrtyrt");
+	 System.out.println("This is Login page");
  }
 public void logoutpage() {
 	System.out.println("This is logout page");
