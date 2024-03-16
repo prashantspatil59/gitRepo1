b10137b01e1080ba1c418760e37016cda257e8de
Prashant Patil <prashantspatil59@gmail.com>
2024-03-16 18:55:48 +0530

    Login page change

--- src/Practice/LoginPage.java
+++ src/Practice/LoginPage.java
@@ -2,7 +2,7 @@ package Practice;
 
 public class LoginPage {
  void loginpage() {
-	 System.out.println("This is login page");
+	 System.out.println("This is login page is not therer");
  }
 public void logoutpage() {
 	System.out.println("This is logout page");
