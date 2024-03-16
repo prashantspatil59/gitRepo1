05265cf09746a88a22e1446b7066279a960489e6
Prashant Patil <prashantspatil59@gmail.com>
2024-03-16 22:52:47 +0530

    perform 32

--- src/Practice/LoginPage.java
+++ src/Practice/LoginPage.java
@@ -2,7 +2,7 @@ package Practice;
 
 public class LoginPage {
  void loginpage() {
-	 System.out.println("This is rhrtyrt");
+	 System.out.println("This is rhrtyr");
  }
 public void logoutpage() {
 	System.out.println("This is logout page");
